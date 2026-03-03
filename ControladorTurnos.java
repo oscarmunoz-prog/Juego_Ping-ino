b1b051847858838cca16bf9d22ca0eb8b370a2de
ian-yepa <ianrubiosanchez17@alumnes.ilerna.com>
2026-03-03 16:25:17 +0100

    Controlador Turnos #5 Comentarios

--- ControladorTurnos.java
+++ ControladorTurnos.java
@@ -3,22 +3,31 @@ package src.JOCDELPINGU.Controladores;
 import src.JOCDELPINGU.Modelo.Jugador;
 import java.util.List;
 
+//Controla la gestion de turnos entre los jugadores
 public class ControladorTurnos {
+
+    //Lista de jugadores que participan en la partida
     private List<Jugador> jugadores;
+
+    //Indice del jugador cuyo turno esta activo
     private int turnoActual;
     
+    //Inicializa el turno en la posicion 0 (primer jugador)
     public ControladorTurnos() {
         this.turnoActual = 0;
     }
      
+     //Asigna la lista de jugadores al controlador
      public void setJugadores(List<Jugador> jugadores) {
         this.jugadores = jugadores;
     }
-
+    
+    //Devuelve el jugador correspondiente al turno actual
      public Jugador getJugadorActual() {
         return jugadores.get(turnoActual);
     }
      
+    //Avanza al siguiente turno de forma ciclica
      public void siguienteTurno() {
         turnoActual = (turnoActual + 1) % jugadores.size();
     }
