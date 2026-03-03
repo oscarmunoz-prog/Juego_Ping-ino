8296dc6e2cda0ef5e2ac77f32956b499ad2c416a
mileagaabriel-dam1 <mileagaabriel@alumnes.ilerna.com>
2026-03-03 16:16:18 +0100

    Modelo AgujeroHielo
    Se han añadido:
    
    Descirpcion de este entidad,
    
    se ha hecho que interactue con el jugador, que genere un numero random entre 0 y 4 de casillas que se retroceden, imprimir por pantalla las casillas que retrocedes, y calcular la nueva posición

--- AgujeroHielo.java
+++ AgujeroHielo.java
@@ -14,3 +14,22 @@ public class AgujeroHielo implements Entidad {
     public String getSimbolo() { return "🕳️"; }
     //Aqui hemos añadido simbolos, para hacerlo más gráfico y bonito, en caso de esta entidad, saldra el simbolo del agujero
     
+    @Override
+    public String getDescripcion() { return " Cuidado con el agujero, te podrías resbalar"; }
+    //Descripción corta de esta entidad
+
+    @Override
+    public void interactuar(Jugador jugador) {
+        //Hacemos que la entidad interactue con el jugador
+
+        int retroceso = (int)(Math.random() * 4) + 1;
+        //Esta entidad hace que retrocedes, las casillas que retrocedes se generaran random entre 0 y 4.
+        System.out.println(" Te has resbalado hacia el agujero");
+        System.out.println("  Retrocedes " + retroceso + " casillas");
+        
+        int nuevaPos = Math.max(0, jugador.getPosicion() - retroceso);
+        jugador.setPosicion(nuevaPos);
+        //Primero se obtiene la posición actual, le resta las casillas que se retrocedes, se calcula, y se obtiene la nueva posición
+
+    }
+}
