5b3e7a7c7f07e56e8a3f87d1fe4018ddccbdb52b
mileagaabriel-dam1 <mileagaabriel@alumnes.ilerna.com>
2026-03-03 15:48:10 +0100

    CREACION DE OBJETOS
    Se crean los objetos y se llaman a las clases necesarias en la CLASE de Controladoreventos.

--- ControladorEventos.java
+++ ControladorEventos.java
@@ -1,9 +1,47 @@
 package src.JOCDELPINGU.Controladores;
+//Define el paquete en donde esta esta clase
 
 import src.JOCDELPINGU.Modelo.*;
+//Importa todas las clases Modelo
 
 public class ControladorEventos {
+//Declaramos la clase publica
     
     public void procesarCasilla(Jugador jugador, Casilla casilla) {
+//Vuelve a declarar clase publica
         Entidad entidad = null;
+    
+    
+        switch(casilla.getTipo()) {
+            //Evalua las casillas mediante getTipo
+           
+            case PINGUINO:
+                entidad = new Pinguino();
+                break;
+            //Si la casilla es pinguino, crear el objeto pinguino
+
+            case OSO:
+                entidad = new Oso();
+                break;
+            //Si la casilla es un Oso, crear esta objeto
+            
+            case AGUJERO:
+                entidad = new AgujeroHielo();
+                break;
+            //Vuelve a ser lo mismo, en caso de, crear el objeto
+
+            case TRINEO:
+                entidad = new Trineo();
+                break;
+            //Crear el objeto trineo
+
+            case INTERROGANTE:
+                eventoAleatorio(jugador);
+                return;
+            //Y en caso de ser un evento aleatorio,, y llamar al eventoAleatorio.
+
+        }
+        
+      
+
         
