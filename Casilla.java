b856cc7dec49a69eefebdf9ca1f383909f375f89
mileagaabriel-dam1 <mileagaabriel@alumnes.ilerna.com>
2026-03-03 16:23:19 +0100

    Modelo Casilla
    Se han añadido todas las descripciones que tocan para cada una de las casillas, y un método getter para obtener el tipo de casilla.

--- Casilla.java
+++ Casilla.java
@@ -29,3 +29,22 @@ public class Casilla {
 
         //Hemos añadido un simbolo para cada tipo de casilla
     }
+    
+    public String getDescripcion() {
+        //Método para obtener la descripción de cada una de las casillas
+
+        switch(tipo) {
+            //Evalua segun el tipo de casilla, y obtiene la descripción correspondiente
+            case PINGUINO: return "Casilla de Pingüino (salida)";
+            case OSO: return "Casilla de Oso, Has vuelto al inicio";
+            case AGUJERO: return "Agujero de hielo - Retrocedes unas casillas";
+            case TRINEO: return "Trineo - Avanzas una buena parte del tablero";
+            case INTERROGANTE: return "Evento Aleatorio";
+            default: return "Casilla normal";
+            //Obtiene la descripción de cada uno, y si es casilla normal, sin ningun evento, ni nada del estilo, devolver "Casilla Normal"
+        }
+    }
+    
+    public TipoCasilla getTipo() { return tipo; }
+}
+//Método "getter" para devolver el tipo de casilla correspondiente.
