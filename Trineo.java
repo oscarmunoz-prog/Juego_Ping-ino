1e32f6f68890b3a3fc7e55bde1df3feb4c431b63
oscarmunoz-prog <oscarmunyozlopez@alumnes.ilerna.com>
2026-03-03 16:40:11 +0100

    Clase Trineo CREADA y Funcion getNombre CREADA

---
+++ Trineo.java
@@ -0,0 +1,10 @@
+package src.JOCDELPINGU.Modelo;
+
+//Declara la clase Trineo que implementa a Entidad
+public class Trineo implements Entidad {
+    @Override
+    //Método que devuelve un String con el nombre
+    public String getNombre() { return "Trineo"; }
+    
+   
+}
