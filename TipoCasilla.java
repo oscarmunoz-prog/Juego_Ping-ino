26aa8ecc78d7e7e1835f93d88fc85d9e02546040
oscarmunoz-prog <oscarmunyozlopez@alumnes.ilerna.com>
2026-03-03 16:48:06 +0100

    Clase TipoCasilla HECHA

---
+++ TipoCasilla.java
@@ -0,0 +1,14 @@
+package src.JOCDELPINGU.Modelo;
+
+public enum TipoCasilla {
+    PINGUINO,
+    //Casilla inicial, pinguino
+    OSO,
+    //Vuelves a la casilla de inicio
+    AGUJERO,
+    //Vas hacia atras
+    TRINEO,
+    //Avanzas hacia delante
+    INTERROGANTE
+    //Como evento aleatorio
+}
