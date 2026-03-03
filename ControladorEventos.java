88b261686c5b6b02f4e24018c02a96bd9614383c
mileagaabriel-dam1 <mileagaabriel@alumnes.ilerna.com>
2026-03-03 15:41:14 +0100

    CREACION CONTROLADOR EVENTOS
    Primera clase de controlador eventos

---
+++ ControladorEventos.java
@@ -0,0 +1,9 @@
+package src.JOCDELPINGU.Controladores;
+
+import src.JOCDELPINGU.Modelo.*;
+
+public class ControladorEventos {
+    
+    public void procesarCasilla(Jugador jugador, Casilla casilla) {
+        Entidad entidad = null;
+        
